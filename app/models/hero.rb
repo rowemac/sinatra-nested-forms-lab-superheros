af8e8a0c7b3f104d0487b6e35722829674f416a3
Mac Rowe <m.rowe42@gmail.com>
2021-04-07 10:10:29 -0400

    commit tree 3e078f1f56ea35be7fb2e42d88800226ac2badd9

---
+++ app/models/hero.rb
@@ -0,0 +1,18 @@
+class Hero
+
+    attr_accessor :name, :power, :biography 
+
+    @@all = [] 
+
+    def initialize(params)
+        @name = params[:name]
+        @power = params[:power]
+        @biography = params[:biography]
+        @@all << self
+    end 
+
+    def self.all
+        @@all
+    end
+
+end
